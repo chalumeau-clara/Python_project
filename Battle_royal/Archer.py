a93af6f304a146b7064a846e5aba813ba996bba3
Robert Nantchouang Nantchouang <robertnantchouangn@gmail.com>
2022-03-11 17:01:58 +0100

    Modification name

--- Battle_royal/Archer.py
+++ Battle_royal/Archer.py
@@ -2,10 +2,11 @@ import Player
 
 
 class Archer(Player):
-    def __init__(self):
+    def __init__(self, name):
         self.class_name = "Archer"
         self.name_attack = "Destructive Arrow"
         self.damage = 70
         self.speed = 9
         self.defense = 3
         self.health = 90
+        self.name = name
