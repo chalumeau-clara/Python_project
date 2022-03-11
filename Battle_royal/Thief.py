c1a7e66e6a6b7662b99cf436dbea7176678aa945
Robert Nantchouang Nantchouang <robertnantchouangn@gmail.com>
2022-03-11 16:58:41 +0100

    Self.name thief

--- Battle_royal/Thief.py
+++ Battle_royal/Thief.py
@@ -2,10 +2,11 @@ import Player
 
 
 class Thief(Player):
-    def __init__(self):
+    def __init__(self, name):
         self.class_name = "Thief"
         self.name_attack = "Furia"
         self.damage = 70
         self.speed = 10
         self.defense = 5
         self.health = 75
+        self.name = name
