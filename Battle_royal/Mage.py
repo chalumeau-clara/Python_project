a3e02db8d5b52d906efb96de55814989fc46b17c
Robert Nantchouang Nantchouang <robertnantchouangn@gmail.com>
2022-03-11 17:01:27 +0100

    New change in name

--- Battle_royal/Mage.py
+++ Battle_royal/Mage.py
@@ -2,10 +2,11 @@ import Player
 
 
 class Mage(Player):
-    def __init__(self):
+    def __init__(self, name):
         self.class_name = "Mage"
         self.name_attack = "Fire spell"
         self.damage = 40
         self.speed = 3
         self.defense = 7
         self.health = 100
+        self.name = name
