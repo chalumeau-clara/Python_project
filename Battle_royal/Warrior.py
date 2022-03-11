a1f3195dc3d3fb4de2b4b7fa5c39f6c51796c6de
Robert Nantchouang Nantchouang <robertnantchouangn@gmail.com>
2022-03-11 16:57:52 +0100

    Add self.name in Warrior

--- Battle_royal/Warrior.py
+++ Battle_royal/Warrior.py
@@ -3,10 +3,11 @@ import random
 
 
 class Warrior(Player):
-    def __init__(self):
+    def __init__(self, name):
         self.class_name = "Warrior"
         self.name_attack = "Strengthstorm"
         self.damage = 50
         self.speed = 7
         self.defense = 2
         self.health = 100
+        self.name = name
