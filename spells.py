5013d5a910d6c61c91b1e3f7a21c1cbf54e4d64f
mickaelchau <mickaelchau4@gmail.com>
2022-03-11 16:19:11 +0100

    Warrior, Mage, Templar, Warrior's spells

---
+++ spells.py
@@ -0,0 +1,5 @@
+warrior_spells = [("Strengthstorm", 50), ("Divine Sword", 50),("Celestial Sword", 40),("Evil Sword", 40),("Fit of Rage", 60), ("Last whisper", 60)]
+archer_spells = [("Destructive Arrow", 60), ("Devouring Arrow", 80),("Explosive Arrow", 20),("Burning Arrow", 10),("Punitive Arrow", 10), ("Abolition Arrow", 120)]
+mage_spells = [("Fire spell", 5), ("Ice spell", 15),("Annihilation Spell", 1000),("Water Spell", 0),("Meteors Spell", 5), ("sentence irrévocable", 10)]
+templar_spells = [("Assault", 15), ("Furia", 10),("Immolation", 20),("Templar's Hostility", 25),("Madness of the templar", 50), ("Absorption", 40)]
+
