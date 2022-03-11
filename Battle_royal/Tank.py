3b4a166f46d22eb7864d86ee5b3da5f24ba2d260
Robert Nantchouang Nantchouang <robertnantchouangn@gmail.com>
2022-03-11 16:59:22 +0100

    Tank self name modification

--- Battle_royal/Tank.py
+++ Battle_royal/Tank.py
@@ -2,10 +2,11 @@ import Player
 
 
 class Tank(Player):
-    def __init__(self):
+    def __init__(self, name):
         self.class_name = "Tank"
         self.name_attack = "Absorption"
         self.damage = 25
         self.speed = 1
         self.defense = 6
         self.health = 150
+        self.name = name
